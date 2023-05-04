833840f4744ecf6ed9ed52fe0083f74d36f641b7
Manjunath Reddy R <47288360+manjunathreddyr@users.noreply.github.com>
2023-05-04 12:30:40 +0530

    Add files via upload

---
+++ solstiswebservice/src/main/java/com/solstis/webservice/SolstisWebserviceLoadtestApplication.java
@@ -0,0 +1,13 @@
+package com.solstis.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolstisWebserviceLoadtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolstisWebserviceLoadtestApplication.class, args);
+	}
+
+}
---
+++ solstiswebservice/src/test/java/com/solstis/webservice/SolstisWebserviceLoadtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solstis.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolstisWebserviceLoadtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
